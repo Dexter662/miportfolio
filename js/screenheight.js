834158efa839da19ec08228411876bc80bea5241
Dexter662 <kuroookami.662@gmail.com>
2022-07-06 20:14:17 -0300

    Mi Portfolio

---
+++ js/screenheight.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var height = $(window).height();
+
+    $('#header').height(height);
+});
+$(document).ready(function(){
+
+    var height = $(window).height();
+
+    $('#sobreMi').height(height);
+});
+$(document).ready(function(){
+
+    var height = $(window).height();
+
+    $('#experiencia').height(height);
+});
+$(document).ready(function(){
+
+    var height = $(window).height();
+
+    $('#contactame').height(height);
+});
